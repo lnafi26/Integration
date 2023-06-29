ca7c58b747e8cfc16503b799414443c561c40c0d
lnafi26 <136921084+lnafi26@users.noreply.github.com>
2023-06-29 15:29:18 -0400

    Add files via upload
    adding Integration.cpp

---
+++ Integration.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+typedef double (*FUNC)(double);
+
+double integra(FUNC f, double x, double y){
+	double incr = .0001, sum = 0;
+	for (double z = x + (incr / 2); z < y; z += incr){
+		sum += f(z) * incr; //calculates area of that rectangle and adds to the total
+	} //iterates through each rectangle
+	return sum;
+}
+
+double single(double x){return x;}
+
+double square(double x){return x * x;}
+
+double cube(double x){return x * x * x;}
+
+int main(){
+	cout << "f(x) = x between 1 and 5 integrated is: " << integra(single, 1, 5) << endl;
+	cout << "f(x) = x^2 between 1 and 5 integrated is: " << integra(square, 1, 5) << endl;
+	cout << "f(x) = x^3 between 1 and 5 integrated is: " << integra(cube, 1, 5) << endl;
+	return 0;
+}
